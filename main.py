ea8aa24e2dac34bc0590147565648852be7ea6b0
PasDaRass <pascalr_dev@outlook.com>
2025-05-12 23:00:01 +0100

    pygame screen setup

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import *
+
+
+
+
+def main():
+    pygame.init()
+    play = True
+    print(f"""
+        Starting Asteroids!
+        Screen width: {SCREEN_WIDTH}
+        Screen height: {SCREEN_HEIGHT}
+          """)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while play:
+        screen.fill("black")
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+    
+
+if (__name__ == "__main__"):
+    main()
